020deda6398ea188563b87ed3dbf190f3b296269
BearTheCoder <bearthecoder@gmail.com>
2022-11-29 23:38:31 -0500

    update

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
+// https://p5js.org/reference/#/p5
+
 let noiseScale = 0.02;
+const resolution = 3;
+let xOffset = 10;
+let yOffset = 0;
+
 function setup () {
- createCanvas(1200, 800);
- background(100);
- line(15, 25, 70, 90);
+  createCanvas(800, 800);
+  background(100);
+  line(15, 25, 70, 90);
 }
 
 function draw () {
- background(0);
- for (let x = 0; x < width; x++) {
-  for (let y = 0; y < array.length; y++) {
-   let noiseVal = noise((x) * noiseScale, Y * noiseScale);
+  for (let x = 0; x < width; x += resolution) {
+    for (let y = 0; y < height; y += resolution) {
+      let noiseVal = noise((x + xOffset) * noiseScale, (y + yOffset) * noiseScale);
+      fill(noiseVal * 255);
+      noStroke();
+      rect(x, y, resolution, resolution);
+    }
 
   }
-
- }
 }
