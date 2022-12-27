026644b86092d1c6d4c457133c00290cb78f0fe2
BearTheCoder <bearthecoder@gmail.com>
2022-12-26 20:16:22 -0500

    update

--- Sagar Particles/index.js
+++ Sagar Particles/index.js
@@ -1,55 +1,41 @@
 //This is ripped directly from the p5js website. Original code be Sagar Arora
 
-// this class describes the properties of a single particle.
-class Particle {
-  // setting the co-ordinates, radius and the
-  // speed of a particle in both the co-ordinates axes.
-  constructor () {
-    this.posX = random(0, width);
-    this.posY = random(0, height);
-    this.radius = random(1, 8);
-    this.xSpeed = random(-2, 2);
-    this.ySpeed = random(-1, 1.5);
-  }
+function Particle () {
+  this.posX = random(0, width);
+  this.posY = random(0, height);
+  this.radius = random(3, 10);
+  this.xSpeed = random(-2, 2);
+  this.ySpeed = random(-1, 1.5);
 
-  // creation of a particle.
-  createParticle () {
+  this.createParticle = () => {
     noStroke();
-    fill('rgba(200,169,169,0.5)');
+    fill('rgba(200,165,165,0.5)');
     circle(this.posX, this.posY, this.radius);
-  }
+  };
 
-  // Move particle in constant direction, move to other side of the screen if past the screen width or height.
-  moveParticle () {
-    if (this.posX < 0 || this.posX > width)
-      this.xSpeed *= -1;
-    if (this.posY < 0 || this.posY > height)
-      this.ySpeed *= -1;
+  this.moveParticle = () => {
+    if (this.posX < 0 || this.posX > width) this.xSpeed *= -1;
+    if (this.posY < 0 || this.posY > height) this.ySpeed *= -1;
     this.posX += this.xSpeed;
     this.posY += this.ySpeed;
-  }
+  };
 
-  // this function creates the connections(lines)
-  // between particles which are less than a certain distance apart
-  joinParticles (particles) {
+  this.joinParticles = (particles) => {
     particles.forEach(element => {
       let dis = dist(this.posX, this.posY, element.posX, element.posY);
-      if (dis < 85) {
-        stroke('rgba(255,255,255,0.04)');
+      if (dis < 120) {
+        stroke('rgba(255,255,255,0.07)');
         line(this.posX, this.posY, element.posX, element.posY);
       }
     });
-  }
+  };
 }
 
-// an array to add multiple particles
 let particles = [];
 
 function setup () {
   createCanvas(1920, 1080);
-  for (let i = 0; i < width / 10; i++) {
-    particles.push(new Particle());
-  }
+  for (let i = 0; i < width / 10; i++) particles.push(new Particle());
 }
 
 function draw () {
