0daaf82f2cd65a53c2101a5589e7ad7a96f1fbae
BearTheCoder <bearthecoder@gmail.com>
2023-02-14 06:03:33 -0500

    update

--- Peter Saville/script.js
+++ Peter Saville/script.js
@@ -1,57 +1,60 @@
-const canvasWidth = 400;
-const canvasHeight = 400;
-const resolution = 10;
-const noiseScale = .05;
-const noiseAmp = 60;
-const startPoint = 60;
+const canvasWidth = 600;
+const canvasHeight = 600;
+const resolution = 5;
+const noiseScale = .02;
+const maxAmp = 150;
+const minAmp = 10;
+const startPoint = 80;
 let xOffset = 0;
 
 function setup () {
   createCanvas(canvasWidth, canvasHeight);
   background(0);
-  drawLines(random(0, canvasWidth));
+  sketch(random(0, canvasWidth));
 }
 
 function draw () {
   background(0);
-  drawLines(xOffset);
+  sketch(xOffset);
   xOffset += 1;
 }
 
-function drawLines (offset) {
-  for (let i = startPoint; i < canvasHeight - startPoint; i += resolution) {
+function sketch (offset) {
+  for (let y = startPoint; y < canvasHeight - startPoint; y += resolution) {
 
+    //Draw Black Infill
     let priorNoise = 0;
-    for (let j = startPoint; j < canvasWidth - startPoint; j++) {
-      let newAmp = getAmplitude(j);
-      const pNoise = noise((j + offset) * noiseScale, i * noiseScale) * newAmp;
-      drawLine(j, i - priorNoise, j, canvasHeight, 0, 2);
+    for (let x = startPoint; x < canvasWidth - startPoint; x++) {
+      let newAmp = getAmplitude(x);
+      const pNoise = noise((x + offset) * noiseScale, y * noiseScale) * newAmp;
+      if (priorNoise === 0) priorNoise = pNoise;
+      drawLine(x, y - priorNoise, x, canvasHeight, 0, 2);
       priorNoise = pNoise;
     }
 
+    // Draw White Line
     priorNoise = 0;
-    for (let j = startPoint; j < canvasWidth - startPoint; j++) {
-      let newAmp = getAmplitude(j);
-      const pNoise = noise((j + offset) * noiseScale, i * noiseScale) * newAmp;
-      drawLine(j, i - priorNoise, j + 1, i - pNoise, 255, 1);
+    for (let x = startPoint; x < canvasWidth - startPoint; x++) {
+      let newAmp = getAmplitude(x);
+      const pNoise = noise((x + offset) * noiseScale, y * noiseScale) * newAmp;
+      if (priorNoise === 0) priorNoise = pNoise;
+      drawLine(x, y - priorNoise, x + 1, y - pNoise, 255, .75);
       priorNoise = pNoise;
     }
   }
 }
 
 function getAmplitude (j) {
-  if (j < (canvasWidth / 4) || j > canvasWidth - (canvasWidth / 4)) {
-    return 0;
-  }
+  if (j < (canvasWidth / 3) || j > canvasWidth - (canvasWidth / 3)) return minAmp;
   if (j <= canvasWidth / 2) {
-    let topNumber = (j - (canvasWidth / 4));
-    let bottomNumber = canvasWidth / 4;
-    return (topNumber / bottomNumber) * noiseAmp;
+    let topNumber = (j - (canvasWidth / 3));
+    let bottomNumber = canvasWidth / 3;
+    return ((topNumber / bottomNumber) * maxAmp) + minAmp;
   }
   else {
-    let topNumber = (((canvasWidth / 2) - (j - (canvasWidth / 2))) - (canvasWidth / 4));
-    let bottomNumber = (canvasWidth / 4);
-    return (topNumber / bottomNumber) * noiseAmp;
+    let topNumber = (((canvasWidth / 2) - (j - (canvasWidth / 2))) - (canvasWidth / 3));
+    let bottomNumber = (canvasWidth / 3);
+    return ((topNumber / bottomNumber) * maxAmp) + minAmp;
   }
 }
 
